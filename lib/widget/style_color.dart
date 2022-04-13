1306fd016eb46ca5454bdb69eb3ebe975a085b04
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-04-13 07:51:11 +0430

    sqflite

---
+++ lib/widget/style_color.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+const Color backGroundColor =  Color(0xFFfffffa);
+const Color primaryColor =  Color(0xFF35bd00);
+const Color iconColor =  Color(0xFF443D4C);
+const Color shadowColor =  Color(0xFFEEEEEE);
+
+//mobile Size
+
+//black
+const bodyBlackMobileStyle=TextStyle(fontSize:16.0 ,color: Colors.black,fontFamily: 'small',);
+const bodyBoldBlackMobileStyle=TextStyle(fontSize:16.0 ,color: Colors.black,fontFamily: 'bold',);
+
+//white
+const bodyWhiteMobileStyle=TextStyle(fontSize:16.0 ,color: Colors.white,fontFamily: 'small',);
+const titleWhiteMobileStyle=TextStyle(fontSize:18,color: Colors.white,fontFamily: 'bold',);
+
+//Green
+const bodyGreenMobileStyle=TextStyle(fontSize: 16.0 ,color: Colors.green,fontFamily: 'small',);
+const bodyBoldGreenMobileStyle=TextStyle(fontSize:16.0 ,color: Colors.green,fontFamily: 'bold',);
+
+//
+const bodyPrimaryMobileStyle=TextStyle(fontSize:16.0 ,color: primaryColor,fontFamily: 'small',);
+const titleBigBlackMobileStyle=TextStyle(fontSize:22.0 ,color: Colors.black,fontFamily: 'bold',);
+//
+//
+//
+//Tablet Size
+
+//black
+const bodyBlackTabletStyle=TextStyle(fontSize:22.0 ,color: Colors.black,fontFamily: 'small',);
+const bodyBoldBlackTabletStyle=TextStyle(fontSize: 22.0 ,color: Colors.black,fontFamily: 'bold',);
+
+//white
+const bodyWhiteTabletStyle=TextStyle(fontSize:22.0 ,color: Colors.white,fontFamily: 'small',);
+const titleWhiteTabletStyle=TextStyle(fontSize:24.0, color: Colors.white,fontFamily: 'bold',);
+
+//Green
+const bodyGreenTabletStyle=TextStyle(fontSize:22.0 ,color: Colors.green,fontFamily: 'small',);
+const bodyBoldGreenTabletStyle=TextStyle(fontSize:22.0 ,color: Colors.green,fontFamily: 'bold',);
+
+//
+const bodyPrimaryTabletStyle=TextStyle(fontSize:22.0 ,color: primaryColor,fontFamily: 'small',);
+const titleBigBlackTabletStyle=TextStyle(fontSize:28.0 ,color: Colors.black,fontFamily: 'bold',);
